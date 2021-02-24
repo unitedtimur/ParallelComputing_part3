b77dc7162c0316d627657a1fef39249b193a2114
unitedtimur <lihomanov.t@inbox.ru>
2021-02-24 22:54:31 +0700

    Release

--- src/filethreadmanager.h
+++ src/filethreadmanager.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <mutex>
 
 namespace ttd {
 using stringlist = std::vector<std::string>;
@@ -14,14 +15,14 @@ class FileThreadManager
 public:
     explicit FileThreadManager() = default;
 
-    ttd::stringlist getFileNames(const std::string &&path) const;
+    ttd::stringlist getFileNames(const std::string &path) const;
 
-    std::string getTextFromFile(const std::string &&fileName) const;
+    std::string getTextFromFile(const std::string &fileName) const;
 
-protected:
+    std::vector<std::string> split(const std::string &input, const std::string &regex);
 
 private:
-
+    mutable std::mutex m_mutex;
 };
 
 #endif // FILETHREADMANAGER_H
